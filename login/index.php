5953c5b13dc490ae73d7a0e7315d8c1fefa73c77
rafsanjane <rafsanhoby@gmail.com>
2023-02-28 00:11:50 +0600

    ..

--- login/index.php
+++ login/index.php
@@ -14,8 +14,34 @@ if (isset($_SESSION['username'])) {
 // Set a cookie
 setcookie("site_language", "en", time() + 3600);
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-// Display the user's username and the cookie value
-echo "<p>Welcome, $username!</p>";
-echo "<p>Your language preference is: " . $_COOKIE['site_language'] . "</p>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="column column-50">
+                <?php
+                // Display the user's username and the cookie value
+                echo "<h1>Welcome, $username!</h1>";
+                echo "<p>Your language preference is: " . $_COOKIE['site_language'] . "</p>";
+
+                ?>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- login/login.php
+++ login/login.php
@@ -28,23 +28,31 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
 </head>
 
 <body>
-    <?php if (isset($error)) : ?>
-        <p><?php echo $error; ?></p>
-    <?php endif; ?>
-    <form method="post">
-        <label>Username:</label>
-        <input type="text" name="username"><br>
-        <label>Password:</label>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="container">
+        <?php if (isset($error)) : ?>
+            <p><?php echo $error; ?></p>
+        <?php endif; ?>
+        <form method="post">
+            <label>Username:</label>
+            <input type="text" name="username"><br>
+            <label>Password:</label>
+            <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
 </body>
 
 </html>
